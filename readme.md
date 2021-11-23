327e4568a4726638d1700744bdecaebc3ec65cf2
Aldovani <aldovane.etec@gmail.com>
2021-11-23 13:11:22 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Desafio 9 - Portfolio
+
+<img src="./.github/portfolio.svg" width="800px" align="center">
+
+<br/><br/>
+
+### 🚀 Tecnologias
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+* [html5](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+* [CSS3](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+* [javaScript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+* [ScrollReveal](https://scrollrevealjs.org/)
+
+<br> 
+
+### 🔖 Layout
+Você pode visualizar o layout do projeto através do link abaixo:
+
+[Layout Web](https://www.figma.com/file/Yb9IBH56g7T1hdIyZ3BMNO/Desafios---Codel%C3%A2ndia?node-id=15409%3A2)
---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+const hamburger = document.querySelector('[data-js="hamburger"]');
+const closeButton =document.querySelector("[data-js='close']")
+const nav = document.querySelector('nav');
+const links = document.querySelectorAll("header nav ul");
+
+hamburger.addEventListener("click", () =>  nav.classList.add("active"));
+closeButton.addEventListener("click", () => nav.classList.remove("active"))
+
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
+ScrollReveal().reveal("#projects div", {
+  interval: 300,
+  distance: "100px",
+  origin: "top",
+  easing: "ease",
+  scale: 0.5,
+  rotate: {
+    z: 15,
+  },
+});
+ScrollReveal().reveal("#services #sites", {
+  distance: "200px",
+  origin: "left",
+  easing: "ease",
+  scale: 2,
+  duration:900,
+});
+ScrollReveal().reveal("#services #design", {
+  duration:900,
+  distance: "200px",
+  origin: "top",
+  easing: "ease",
+  scale: 2,
+});
+ScrollReveal().reveal("#services #smartphone", {
+  duration:900,
+  distance: "200px",
+  origin: "right",
+  easing: "ease",
+  scale: 2,
+});
